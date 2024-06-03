f0ee2d15f5a7c3cb7613ff6c0edfec877cfb9608
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-06-03 19:05:10 -0300

    desafio 1 - new feature

--- desafio_01/assets/js/script.js
+++ desafio_01/assets/js/script.js
@@ -1,15 +1,87 @@
 const newsContainer = document.querySelectorAll('.news-container');
 const iconHeart = document.querySelectorAll('.icon-heart');
+const inputSearch = document.getElementById('input-search');
+const allTitlesNews = document.querySelectorAll('.news-container__title');
 
-// * container and icon styles when clicking on it
 
-iconHeart.forEach(icon => {
-    icon.addEventListener('click', () => {
-        icon.classList.toggle('fa-regular');
-        icon.classList.toggle('fa-solid');
+
+// Carregar notícias favoritas do localStorage ao iniciar a página
+
+document.addEventListener('DOMContentLoaded', () => {
+    const savedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
+
+    savedFavorites.forEach(id => {
+
+        const favoriteNews = document.querySelector(`.news-container[data-id='${id}']`);
+
+        if (favoriteNews) {
+            const heartIcon = favoriteNews.querySelector('.icon-heart');
+
+            heartIcon.classList.add('fa-solid');
+            heartIcon.classList.remove('fa-regular');
+
+            favoriteNews.classList.add('border');
+        }
     });
+
 });
 
-newsContainer.forEach(news => {
-    news.addEventListener('click', () => news.classList.toggle('border'))
+
+// Função para salvar ou remover notícias do localStorage
+
+const updateLocalStorage = (id, isFavorite) => {
+
+    let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+
+    if (isFavorite) {
+        if (!favorites.includes(id)) {
+            favorites.push(id);
+        }
+    } else {
+        favorites = favorites.filter(favId => favId !== id);
+    }
+
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+
+};
+
+
+// Estilo do container e do ícone quando clicar nele
+
+iconHeart.forEach(icon => {
+
+    icon.addEventListener('click', () => {
+
+        const clickedIconContainer = icon.parentElement.parentElement;
+        const newsId = clickedIconContainer.getAttribute('data-id');
+
+        icon.classList.toggle('fa-regular');
+        icon.classList.toggle('fa-solid');
+
+        clickedIconContainer.classList.toggle('border');
+
+        const isFavorite = icon.classList.contains('fa-solid');
+
+        updateLocalStorage(newsId, isFavorite);
+
+    });
+
+});
+
+
+// Filtrar notícias com base no que o usuário digita no campo de input
+
+inputSearch.addEventListener('input', () => {
+
+    const inputSearchValue = inputSearch.value.toLowerCase();
+
+    allTitlesNews.forEach(title => {
+
+        const containerNew = title.parentElement.parentElement;
+
+        title.textContent.toLowerCase().includes(inputSearchValue) ?
+            containerNew.style.display = 'block' :
+            containerNew.style.display = 'none';
+
+    });
 });
