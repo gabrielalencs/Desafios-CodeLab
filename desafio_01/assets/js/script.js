52442089aac4d8186ad288f6d7feeccb6893e723
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-06-03 18:21:34 -0300

    desafio 1 - new feature

--- desafio_01/assets/js/script.js
+++ desafio_01/assets/js/script.js
@@ -1,2 +1,15 @@
 const newsContainer = document.querySelectorAll('.news-container');
-const iconHeart = document.querySelectorAll('.icon-heart')
+const iconHeart = document.querySelectorAll('.icon-heart');
+
+// * container and icon styles when clicking on it
+
+iconHeart.forEach(icon => {
+    icon.addEventListener('click', () => {
+        icon.classList.toggle('fa-regular');
+        icon.classList.toggle('fa-solid');
+    });
+});
+
+newsContainer.forEach(news => {
+    news.addEventListener('click', () => news.classList.toggle('border'))
+});
