c66056cbd522b9f825ff772cdb89c152033e020a
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-06-13 21:17:35 -0300

    projeto 2 - seção inicial

--- desafio_02/tailwind.config.js
+++ desafio_02/tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
             'primary-color': '#7CA2F4',
             'black-primary': '#121214',
             'gray-primary': '#F3F7FF'
+        },
+        spacing: {
+            '100': '30rem',
+            '1216': '1216px'
         }
     },
   },
