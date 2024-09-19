3ff2394e86c430c968b8610976189506bad49f07
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-09-18 23:00:58 -0300

    desafio 09 - seção hero da página

--- desafio_09/src/App.jsx
+++ desafio_09/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css';
 
+// Components
+
 import Header from './components/Header';
+import HeroSection from './components/HeroSection';
 
 
 const App = () => {
     return (
         <>
             <Header />
+            <main>
+                <HeroSection />
+            </main>
         </>
     )
 }
